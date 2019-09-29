4aabf6d549ef1ce7ac1eea909d60e205cb61d1a2
João Marcus <joaomarcus12769@gmail.com>
2019-09-29 18:55:18 -0300

    Adicionando configurações do item 2

--- interface-serial/interface-serial/main.c
+++ interface-serial/interface-serial/main.c
@@ -5,13 +5,17 @@
  * Author : João Marcus e Nivardo
  */ 
 
+#ifndef F_CPU
+#define F_CPU 8000000UL
+#endif
+#define FOSC 16000000
+#define BAUD 28800
+#define BRC FOSC/16/BAUD-1
+
+#include <util/delay.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <string.h>
-#define F_CPU 1000000
-#include <util/delay.h>
-#define BAUD 4800
-#define BRC ((F_CPU/16/BAUD)-1)
 
 void enviaDado(char dado)
 {
@@ -45,9 +49,10 @@ void USART_init(unsigned int ubrr)
 {	
 	UBRR0H = (unsigned char)(ubrr>>8);
 	UBRR0L = (unsigned char)ubrr;
-	UCSR0B |= (1<<RXEN0)|(1<<TXEN0);
+	UCSR0A |= (1 << U2X0);
+	UCSR0B |= (1 << RXEN0)|(1 << TXEN0);
 	UCSR0B |= (1 << RXCIE0);
-	UCSR0C |= (1<<UCSZ01)|(1<<UCSZ00);
+	UCSR0C = 0x2C; //7-bits paridade:par stopbit:2
 }
 
 volatile char byteRecebido;
@@ -55,7 +60,7 @@ volatile char byteRecebido;
 ISR(USART_RX_vect)
 {
 	byteRecebido = UDR0;
-	UDR0 = byteRecebido;
+	//UDR0 = byteRecebido;
 	enviaDado(byteRecebido);
 }
 
